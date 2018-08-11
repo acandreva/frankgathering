7048356ac4d7a32c6ba9b617aef6252f18ab616b
Allison Candreva <allisonlogan@Allisons-MacBook-Pro-2.local>
2018-08-11 16:45:37 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!--BEGINNING OF FOOTER AREA - END SECTION CONTENT-->
+        <footer class="row">
+            <div class="four columns">
+                <?php dynamic_sidebar('footer-widget-one'); ?>
+            </div>
+            <div class="four columns">
+                <?php dynamic_sidebar('footer-widget-two'); ?>
+            </div>
+            <div class="four columns">
+                <?php dynamic_sidebar('footer-widget-three'); ?>
+            </div>
+        </footer>
+    </div><!--end container div-->
+<?php wp_footer(); ?>
+</body>
+</html>
