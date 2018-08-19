1278d8413d7a7e66ae045b84595aa83a2a81fa07
Allison Candreva <allisonlogan@Allisons-MacBook-Pro-2.local>
2018-08-18 22:52:03 -0400

    modifies footer and overall divs

--- footer.php
+++ footer.php
@@ -1,21 +1,23 @@
 <!--BEGINNING OF FOOTER AREA - END SECTION CONTENT-->
-        <footer class="row">
-            <div class="four columns">
-				<nav role="navigation" aria-label="<?php _e( 'Footer Menu', 'frank-gathering' ); ?>">
-				<?php wp_nav_menu(array(
-					'theme_location' => 'footer-menu',
-					'depth' => 1, 
+    <footer class="row">
+        <div class="footer-content">
+            <div class="eight columns">
+                <nav role="navigation" aria-label="<?php _e( 'Footer Menu', 'frank-gathering' ); ?>">
+                <?php wp_nav_menu(array(
+                    'theme_location' => 'footer-menu',
+                    'depth' => 1, 
                 )); ?> 
                 </nav>         
             </div>
-            <div class="four columns">
-                <?php dynamic_sidebar('footer-widget-one'); ?>
+            <div class="four columns footer-right">
+                <div class="footer-right-content">
+                <?php dynamic_sidebar('footer-widget'); ?>
+                <img src="http://www.allisoncandreva.com/test/wp-content/themes/frank/images/franklogo.png" title="frank logo" alt="An image of the frank logo." />
+                <img src="http://www.allisoncandreva.com/test/wp-content/themes/frank/images/uf_signature.png" title="UF Signature" alt="An image of the UF signature." />
+                </div>
             </div>
-            <div class="four columns">
-                <?php dynamic_sidebar('footer-widget-two'); ?>
-            </div>
-        </footer>
-    </div><!--end container div-->
+        </div><!--end footer-content div-->
+    </footer>
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -5,38 +5,36 @@
 <?php wp_head(); ?>
 </head>
 <body>
-    <div class= "container">
-      <header> 
-        <div class="row siteheader">
-            <div class="three columns siteinfo">
-                <a href="https://allisoncandreva.com/test">
-                    <h1><?php bloginfo('name') ?></h1>
-                    <p><?php bloginfo('description') ?></p>
-                </a>
-			</div>
-            <div class="nine columns sitenav">
-				<nav role="navigation" aria-label="<?php _e( 'Main Menu', 'frank-gathering' ); ?>" >
-                <?php wp_nav_menu(array(
-					'theme_location' => 'main-menu',
-					'depth' => 2, 
-                    'container_class' => 'main-menu-header'
-                )); ?>
-                </nav>
-	
-				<?php if ( has_nav_menu( 'social-links' ) ) : ?>
-            	<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links', 'frank-gathering' ); ?>">
-				<?php wp_nav_menu(array(
-                        'theme_location' => 'social-links',
-                        'depth'          => 1,
-                        'container_class'     => 'social-links-menu',
-						'link_before'    => '<span class="screen-reader-text">',
-						'link_after'     => '</span>',                 
-				 ) ); ?>
-                <?php get_search_form(); ?>
-                 </nav>
-				<?php endif; ?>
-            </div>
-        </div><!--closes header-container-->
-      </header>
-
+    <header> 
+      <div class="row siteheader">
+          <div class="three columns siteinfo">
+              <a href="https://allisoncandreva.com/test">
+                  <h1><?php bloginfo('name') ?></h1>
+                  <p><?php bloginfo('description') ?></p>
+              </a>
+          </div>
+          <div class="nine columns sitenav">
+              <nav role="navigation" aria-label="<?php _e( 'Main Menu', 'frank-gathering' ); ?>" >
+              <?php wp_nav_menu(array(
+                  'theme_location' => 'main-menu',
+                  'depth' => 2, 
+                  'container_class' => 'main-menu-header'
+              )); ?>
+              </nav>
+  
+              <?php if ( has_nav_menu( 'social-links' ) ) : ?>
+              <nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links', 'frank-gathering' ); ?>">
+              <?php wp_nav_menu(array(
+                      'theme_location' => 'social-links',
+                      'depth'          => 1,
+                      'container_class'     => 'social-links-menu',
+                      'link_before'    => '<span class="screen-reader-text">',
+                      'link_after'     => '</span>',                 
+               ) ); ?>
+              <?php get_search_form(); ?>
+               </nav>
+              <?php endif; ?>
+          </div>
+      </div><!--closes siteheader-->
+    </header>
 <!--END OF HEADER AREA - BEGIN SECTION CONTENT-->
