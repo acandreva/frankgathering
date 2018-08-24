28bbcce688ad6731bafe13c85c2e2fa7b78ceb05
Allison Candreva <allisonlogan@Allisons-MacBook-Pro-2.local>
2018-08-23 20:32:25 -0400

    adds menu warning

--- functions.php
+++ functions.php
@@ -66,4 +66,36 @@ function wpdocs_theme_setup() {
 /*-------------- Enable Post Thumbnails--------------- */
 add_theme_support('post-thumbnails');
 
+/*-------------- Menu Limit--------------- */
+
+function custom_admin_js() {
+$screen = get_current_screen();
+if ( $screen->id != 'nav-menus' )
+	return;
+?>
+<script type="text/javascript">
+	jQuery(function($){
+	var warn = false;
+	$('.submit-add-to-menu').on('click', function(){
+	var num_menu = $('#menu-to-edit').children('.menu-item-depth-0').length;
+	num_menu += $('.menu-item-checkbox:checked').length;
+	if ( !warn && ( num_menu > 7 ) ) {
+	warn = true;
+	alert('warning: Top level menu Items are limited to 7!');
+	
+	}
+	});
+	$('#update-nav-menu').on('submit', function(){
+	var num_menu = $('#menu-to-edit').children('.menu-item-depth-0').length;
+	if ( num_menu > 7 ) {
+	alert('error: Top level menu Items are limited to 7!');
+	return false;
+	}
+	});
+	})
+</script>
+<?php
+	}
+	add_action('admin_footer', 'custom_admin_js');
+
 ?>
