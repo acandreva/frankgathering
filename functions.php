172a7a120714df2b15ae96f32ea7b53ab4aa001f
Allison Logan <allisonlogan@Logan-Family-iMac.local>
2018-08-25 09:50:41 -0400

    working on hamburger for mobile

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
 
 function add_theme_scripts() {
   wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'jquery.sweet-dropdown.min', get_stylesheet_uri() );
+  wp_enqueue_script( 'jquery.sweet-dropdown.min', get_template_directory_uri() . '/js/jquery.sweet-dropdown.min.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
