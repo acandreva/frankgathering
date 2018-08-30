e4ed8baa714b8913a0f25e095d55f6d0050e1e54
Allison Logan <allisonlogan@Logan-Family-iMac.local>
2018-08-29 21:45:10 -0400

    works on custom tabs

---
+++ plugins/Anchor-Links-Demo/anchorlinks.js
@@ -0,0 +1,9 @@
+jQuery(function ($) {
+
+    var open_tab_anchor = $('#menu-item-143 a');
+    open_tab_anchor.on('click', function(event){
+        $('#event-tabs .first-tab a').click();
+        $("html, body").animate({ scrollTop: $('#event-tabs').offset().top }, 1000);
+    });
+    
+});
