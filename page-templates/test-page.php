18ab12ff571d03ff92cd248195f652edd5503f5c
Allison Logan <allisonlogan@ufl.edu>
2018-08-31 12:59:21 -0400

    fixes hover on tabs

--- page-templates/test-page.php
+++ page-templates/test-page.php
@@ -32,7 +32,7 @@ get_header(); ?>
         <div class="row">
         	<div id="event-tabs" class="twelve columns">
 				<input id="tab1" type="radio" name="tabs" class="first-tab" checked>
-				<label for="tab1"><h1 id="tab1-title"><em>frank</em> 2019 </h1></label>
+				<label for="tab1"><h1><em>frank</em> 2019 </h1></label>
 
 				<input id="tab2" type="radio" name="tabs">
 				<label for="tab2"><h1><?php the_field( 'event_second_tab' ); ?></h1></label>
@@ -65,16 +65,4 @@ get_header(); ?>
         	</div>
    		</div>         
 	</div><!--end container div-->
-<script>
-	document.getElementById("tab1-title").onmouseover = function() {mouseOver()};
-	document.getElementById("tab1-title").onmouseout = function() {mouseOut()};
-
-	function mouseOver() {
-		document.getElementById("tab1-title").style.color = "rgb(197, 78, 144)";
-	}
-
-	function mouseOut() {
-		document.getElementById("tab1-title").style.color = "rgb(187, 187, 187)";
-	}
-</script>
 <?php get_footer(); ?>
