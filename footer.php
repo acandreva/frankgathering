95d920452eaf204bd28aed11a81ae20e07e433cb
Allison Candreva <allisonlogan@Allisons-MacBook-Pro-2.local>
2018-09-08 22:10:19 -0400

    adds mobile styles to footer

--- footer.php
+++ footer.php
@@ -1,23 +1,21 @@
 <!--BEGINNING OF FOOTER AREA - END SECTION CONTENT-->
-    <footer class="row">
-        <div class="footer-content">
-            <div class="eight columns">
-                <nav role="navigation" aria-label="<?php _e( 'Footer Menu', 'frank-gathering' ); ?>">
-                <?php wp_nav_menu(array(
-                    'theme_location' => 'footer-menu',
-                    'depth' => 1, 
-                )); ?> 
-                </nav>         
+    <footer class="row footer-content">
+        <div class="six columns footer-left">
+            <nav role="navigation" aria-label="<?php _e( 'Footer Menu', 'frank-gathering' ); ?>">
+            <?php wp_nav_menu(array(
+                'theme_location' => 'footer-menu',
+                'depth' => 1, 
+            )); ?> 
+            </nav>         
+        </div>
+        <div class="six columns footer-right">
+            <div class="footer-right-content">
+            <?php dynamic_sidebar('footer-widget'); ?>
+            <img src="http://www.allisoncandreva.com/test/wp-content/themes/frank/images/franklogo.png" title="frank logo" alt="An image of the frank logo." />
+            <a href="http://www.ufl.edu/" target="_blank"<img src="http://www.allisoncandreva.com/test/wp-content/themes/frank/images/uf_signature.png" title="UF Signature" alt="An image of the UF signature." /></a>
             </div>
-            <div class="four columns footer-right">
-                <div class="footer-right-content">
-                <?php dynamic_sidebar('footer-widget'); ?>
-                <img src="http://www.allisoncandreva.com/test/wp-content/themes/frank/images/franklogo.png" title="frank logo" alt="An image of the frank logo." />
-                <img src="http://www.allisoncandreva.com/test/wp-content/themes/frank/images/uf_signature.png" title="UF Signature" alt="An image of the UF signature." />
-                </div>
-            </div>
-        </div><!--end footer-content div-->
-    </footer>
+        </div>
+    </footer><!--end footer-content-->
 <?php wp_footer(); ?>
 </body>
 </html>
