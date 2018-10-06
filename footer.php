d6adfb6e4863211914cbb8b37b285cdc558d5b84
Allison Logan <allisonlogan@Logan-Family-iMac.local>
2018-10-05 21:07:08 -0400

    makes footer more customizable

--- footer.php
+++ footer.php
@@ -12,8 +12,6 @@
 			<div class="six columns footer-right">
 				<div class="footer-right-content">
 				<?php dynamic_sidebar('footer-widget'); ?>
-				<img src="http://www.allisoncandreva.com/test/wp-content/themes/frank/images/franklogo.png" title="frank logo" alt="An image of the frank logo." />-->
-				<a href="http://www.ufl.edu/" target="_blank"><img src="http://www.allisoncandreva.com/test/wp-content/themes/frank/images/uf_signature.png" title="UF Signature" alt="An image of the UF signature." /></a>
 				</div>
 			</div>
 		</div>
