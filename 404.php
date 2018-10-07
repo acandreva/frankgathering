e78a7b53c06b025eefa3191a9c95e6e6291e48f7
Allison Logan <allisonlogan@Logan-Family-iMac.local>
2018-10-06 20:35:42 -0400

    updates 404 page

--- 404.php
+++ 404.php
@@ -5,18 +5,11 @@ get_header(); ?>
     <div class="container">
 		<div class="row">
             <div class="twelve columns error-pg">
-            	<img src="https://allisoncandreva.com/test/wp-content/themes/frank/images/404bg.png" alt="" />
-            	<!--<div class="full-img-bg" style="background-image: url(https://allisoncandreva.com/test/wp-content/themes/frank/images/404bg.png)">
-    	<span role="img" aria-label="A woman attends frank 2018"> </span>
-	</div>-->
+            	<h2>404</h2>
+				<h3>Page not found.</h3>
+				<p>The page you're looking for does not exist or another error occured.</p>
+				<p><a href="javascript:history.back()">Go back</a> or go to the <a href="http://frank.jou.ufl.edu/"><em>frank</em> home page.</a></p>
     	    </div>
-        </div>
-        <div class="row">
-            <div class="twelve columns">
-                <h2>Not Found</h2>
-                <p>This is somewhat embarrassing, isn't it?</p>
-                <p>It looks like nothing was found at this location.</p>
-            </div>
         </div>
     </div>
 <?php get_footer(); ?>
